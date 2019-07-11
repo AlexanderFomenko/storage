4a802f89834073f8e4788840813a9d5b8023a2ff
root <root@testproject.testproject>
2019-07-11 12:17:28 -0400

    TABLE_INFORMATION

--- main/views.py
+++ main/views.py
@@ -3,11 +3,12 @@ from rest_framework import generics
 from rest_framework.response import Response
 from .utils import ls
 from utils.exec import Executor
+import json
 
 class GetPussy(generics.ListAPIView):
     def get(self, request):
-        ans = Executor.exec('ip a').split('\n')
-        return Response({'kek': ans})
+        # ans = Executor.exec('ip -j a').split('\n')
+        return Response({'eth1':['12','10','3'],'eth2':['11','4','8']})
 
 def index(request):
     return render(request, 'main/index.html')
--- react-boilerplate/src/components/App.js
+++ react-boilerplate/src/components/App.js
@@ -6,72 +6,92 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            result: "",
-            a: 1,
-            b: 2,
             table: null
-        }
-        this.enterNumber = this.enterNumber.bind(this);
+
+        };
+        Object.prototype.map = function (func) {
+            const new_arr = [];
+            for(let i in this){
+
+                if(this.hasOwnProperty(i)){
+                    new_arr.push(func(this[i], i));
+                }
+            }
+
+            return new_arr;
+        };
+        // this.enterNumber = this.enterNumber.bind(this);
     }
 
-    enterNumber(ev){
-        let result = this.state.result;
-        const target = ev.currentTarget;
-        result = result + target.innerText;
-        this.setState({
-            result: result
-        });
-    }
+    // enterNumber(ev){
+    //     let result = this.state.result;
+    //     const target = ev.currentTarget;
+    //     result = result + target.innerText;
+    //     this.setState({
+    //         result: result
+    //     });
+    // }
 
     componentDidMount() {
+        // const a = fetch('/main/get').then(ans => ans.json());
+        // console.log(a);
         fetch('/main/get').then(ans => ans.json())
             .then(res => {
-                console.log(res);
                 this.setState({
-                    result: res.kek[0],
-                    table: res.kek
+                    table: res
                 })
             });
     }
+
+
 
     render() {
         return (
             <div>
                 <h1>My React App 4!</h1>
                 <div className="calc">
-                    <div>Результат: <span>{this.state.result}</span></div>
-                    <table border={1}>
-                        <tbody>
-                        <tr>
-                            <td onClick={this.enterNumber}>
-                                1
-                            </td>
-                            <td onClick={this.enterNumber}>2</td>
-                            <td onClick={this.enterNumber}>3</td>
-                        </tr>
-                        <tr>
-                            <td onClick={this.enterNumber}>4</td>
-                            <td onClick={this.enterNumber}>5</td>
-                            <td onClick={this.enterNumber}>6</td>
-                        </tr>
-                        <tr>
-                            <td onClick={this.enterNumber}>7</td>
-                            <td onClick={this.enterNumber}>8</td>
-                            <td onClick={this.enterNumber}>9</td>
-                        </tr>
-                        <tr>
-                            <td onClick={this.enterNumber}>0</td>
-                            <td onClick={this.enterNumber}>+</td>
-                            <td onClick={this.enterNumber}>-</td>
-                        </tr>
-                        </tbody>
-                    </table>
+                    {/*<div>Результат: <span>{this.state.result}</span></div>*/}
+                    {/*<table border={1}>*/}
+                    {/*    <tbody>*/}
+                    {/*    <tr>*/}
+                    {/*        <td onClick={this.enterNumber}>*/}
+                    {/*            1*/}
+                    {/*        </td>*/}
+                    {/*        <td onClick={this.enterNumber}>2</td>*/}
+                    {/*        <td onClick={this.enterNumber}>3</td>*/}
+                    {/*    </tr>*/}
+                    {/*    <tr>*/}
+                    {/*        <td onClick={this.enterNumber}>4</td>*/}
+                    {/*        <td onClick={this.enterNumber}>5</td>*/}
+                    {/*        <td onClick={this.enterNumber}>6</td>*/}
+                    {/*    </tr>*/}
+                    {/*    <tr>*/}
+                    {/*        <td onClick={this.enterNumber}>7</td>*/}
+                    {/*        <td onClick={this.enterNumber}>8</td>*/}
+                    {/*        <td onClick={this.enterNumber}>9</td>*/}
+                    {/*    </tr>*/}
+                    {/*    <tr>*/}
+                    {/*        <td onClick={this.enterNumber}>0</td>*/}
+                    {/*        <td onClick={this.enterNumber}>+</td>*/}
+                    {/*        <td onClick={this.enterNumber}>-</td>*/}
+                    {/*    </tr>*/}
+                    {/*    </tbody>*/}
+                    {/*</table>*/}
                     <table>
                         {
-                            this.state.table ?
-                                this.state.table.map( el =>
-                                    <tr><td>{el}</td></tr>):
-                                <tr><td>Зогрузка....</td></tr>
+                            console.log(this.state.table)
+                            // this.state.table ?
+                            //     this.state.table.map( (el, key) =>
+                            //         <tr>
+                            //             <td>Head: {key}</td>
+                            //             {
+                            //                 el.map( el2 =>
+                            //                     <td>{el2}</td>
+                            //                 )
+                            //             }
+                            //         </tr>
+                            //     ) :
+                            //     <tr><td>Зогрузка....</td></tr>
                         }
                     </table>
 
